6a9c2185090d464f8d3252f53e546d6d806f3d0c
Tushar  Borole <tushariscoolster@gmail.com>
2016-11-22 23:35:30 -0600

    Create WelcomMat.py

---
+++ WelcomMat.py
@@ -0,0 +1,6 @@
+N, M = map(int,raw_input().split()) # More than 6 lines of code will result in 0 score. Blank lines are not counted.
+for i in xrange(1,N,2):
+    print "-" * ((M-(i * 3))/2) + str(i * ".|.") + "-" * ((M-(i * 3))/2)
+print ((M-7)/2) * "-"+"WELCOME"+((M-7)/2) * "-"
+for i in xrange(N-2,-1,-2):
+    print "-" * ((M-(i * 3))/2) + str(i * ".|.") + "-" * ((M-(i * 3))/2)
